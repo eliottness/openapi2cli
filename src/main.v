50058d5cb3c0e5e88648d02fd0271862d27d7df8
Eliott Bouhana <eliott.bouhana@epita.fr>
2022-03-07 14:41:16 +0100

    moved code to src

--- src/main.v
+++ src/main.v
@@ -3,14 +3,6 @@ module main
 import os
 import flag
 
-fn build(path string) string {
-	println(path)
-	// fp := os.open_file(path, "r") or {
-	// 	panic("Cannot open OpenAPI Spec at path: $path")
-	// }
-	return path
-}
-
 fn main() {
 	mut fp := flag.new_flag_parser(os.args)
 
@@ -19,13 +11,15 @@ fn main() {
 	fp.description('A sample CLI application that prints geometric shapes to the console.')
 	fp.skip_executable()
 
+	debug := fp.bool("debug", 0, false, "Toggle Debug mode")
+
 	args := fp.finalize() ?
 	if args.len != 1 {
-		println("USAGE: ./openapi2cli <path to openapi file>")
+		println('USAGE: ./openapi2cli <path to openapi file>')
 		return
 	}
 
 	yaml_filepath := args[0] ?
-	v_filepath := build(yaml_filepath)
-	os.execvp("v", ["build", v_filepath, ]) ?
+	v_filepath := build(yaml_filepath, debug) ?
+	os.execvp('v', ['build', v_filepath]) ?
 }
