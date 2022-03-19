0742d57825ce1ac4211290a3d9421c1182363925
Leiyks <rulleaualexandre@gmail.com>
2022-03-19 14:44:07 +0100

    format

--- src/open_api/openapi_struct_test.v
+++ src/open_api/openapi_struct_test.v
@@ -6,22 +6,22 @@ import os
 fn test_basic_open_api_struct() ? {
 	content := os.read_file('./src/open_api/testdata/open_api_basic.json') ?
 	open_api_obj := json2.decode<OpenApi>(content) ?
-	assert open_api_obj.openapi == "3"
-	assert open_api_obj.info.title == "Sample Pet Store App"
-	assert open_api_obj.info.version == "1.0.1"
-	
+	assert open_api_obj.openapi == '3'
+	assert open_api_obj.info.title == 'Sample Pet Store App'
+	assert open_api_obj.info.version == '1.0.1'
+
 	assert open_api_obj.paths.len == 1
-	assert "/home" in open_api_obj.paths
-	assert open_api_obj.paths["/home"].description == "homepage"
-	
+	assert '/home' in open_api_obj.paths
+	assert open_api_obj.paths['/home'].description == 'homepage'
+
 	assert open_api_obj.tags.len == 2
-	assert open_api_obj.tags[0].name == "cat"
-	assert open_api_obj.tags[1].name == "dog"
+	assert open_api_obj.tags[0].name == 'cat'
+	assert open_api_obj.tags[1].name == 'dog'
 
 	assert open_api_obj.servers.len == 1
-	assert open_api_obj.servers[0].url == "https://random.fr"
+	assert open_api_obj.servers[0].url == 'https://random.fr'
 	assert open_api_obj.servers[0].variables.len == 1
-	assert open_api_obj.servers[0].variables['var1'].default_value == "default" 
+	assert open_api_obj.servers[0].variables['var1'].default_value == 'default'
 }
 
 fn test_open_api_struct_without_required() ? {
