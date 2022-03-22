2f1b6edee25b5001c45671f807fd10bee1a771a0
alexandre.rulleau <rulleaualexandre@gmail.com>
2022-03-22 18:20:53 +0100

    nit

--- open_api/callback.v
+++ open_api/callback.v
@@ -1,15 +1,14 @@
 module open_api
 
 import x.json2 { Any, decode }
+import json
 
 type Callback = map[string]PathItem
 
 pub fn (mut callback Callback) from_json(json Any) {
-	mut tmp := map[string]PathItem
+	mut tmp := map[string]PathItem{}
 	for key, value in json.as_map() {
-		tmp[key] = decode<PathItem>(value.json_str()) or {
-			panic('Failed Callback decoding: $err')
-		}
+		tmp[key] = decode<PathItem>(value.json_str()) or { panic('Failed Callback decoding: $err') }
 	}
 	callback = tmp
 }
