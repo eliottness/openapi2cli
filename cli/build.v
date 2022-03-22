66ead088a470eb5b7386239214cac65e2594e6d4
Leiyks <rulleaualexandre@gmail.com>
2022-03-22 23:29:18 +0100

    Use new decode func in build.v

--- cli/build.v
+++ cli/build.v
@@ -1,13 +1,12 @@
 module cli
 
 import os
-import x.json2
 import yaml
 import open_api
 
 pub fn build(path string, debug bool) ?string {
 	content := os.read_file(path) ?
 	raw_json := yaml.yaml_to_json(content, replace_tags: true, debug: int(debug)) ?
-	json_data := json2.decode<open_api.OpenApi>(raw_json) ?
+	json_data := open_api.decode<open_api.OpenApi>(raw_json) ?
 	return json_data.str()
 }
