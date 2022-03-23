b6f1f8f836d525cc93141af9490049a3a180cbdd
Alexandre Rulleau <55387832+Leiyks@users.noreply.github.com>
2022-03-23 12:33:36 +0100

    Add verification on Info struct (#8)

--- open_api/helpers.v
+++ open_api/helpers.v
@@ -2,6 +2,7 @@ module open_api
 
 import x.json2 { Any, raw_decode }
 import json
+import regex
 
 pub fn decode<T>(src string) ?T {
 	res := raw_decode(src) ?
@@ -64,10 +65,6 @@ pub fn decode_map_any(src string) ?map[string]Any {
 	return typ
 }
 
-fn fake_predicat(str string) bool {
-	return true
-}
-
 pub fn decode_map_sumtype<T>(src string, verif fn (string) bool) ?map[string]ObjectRef<T> {
 	json := raw_decode(src) ?
 	mut typ := map[string]ObjectRef<T>{}
@@ -86,4 +83,29 @@ pub fn check_required<T>(object map[string]Any, required_fields ...string) ? {
 			return error('Failed $T.name decoding: "$field" not specified !')
 		}
 	}
+}
+
+// ---------------------------------------- //
+
+fn fake_predicat(str string) bool {
+	return true
+}
+
+fn check_key_regex(str string) bool {
+	mut reg := regex.regex_opt(r'^[\w\.\-]+$') or { panic('Failed to initialize regex expression') }
+	return reg.matches_string(str)
+}
+
+fn check_url_regex(str string) bool {
+	mut reg := regex.regex_opt(r'^(https?://)?(www\.)?[\w\-@:%\+~#=]{2,256}\.[\a]{2,6}[\-\w@:%\+~#?&//=\.]*$') or {
+		panic('Failed to initialize regex expression')
+	}
+	return reg.matches_string(str)
+}
+
+fn check_email_regex(str string) bool {
+	mut reg := regex.regex_opt(r'^\S+@\S+\.\S+$') or {
+		panic('Failed to initialize regex expression')
+	}
+	return reg.matches_string(str)
 }
