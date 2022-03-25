5a337965a3fb76910e2e79cb0843cf9cc3e3615b
Leiyks <rulleaualexandre@gmail.com>
2022-03-25 01:29:57 +0100

    nit

--- open_api/tests/openapi_test.v
+++ open_api/tests/openapi_test.v
@@ -48,7 +48,9 @@ fn escape_escaped_char(str string) ?string {
 
 	mut reg := regex.regex_opt(r'\\[a-zA-Z]') ?
 	for char in reg.find_all_str(str) {
-		if char in checked { continue }
+		if char in checked {
+			continue
+		}
 		tmp = tmp.replace(char, '\\$char')
 		checked << char
 	}
