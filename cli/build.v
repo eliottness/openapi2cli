6ec03eef5b80c8e789ff5affd2a81026c6e026d1
Leiyks <rulleaualexandre@gmail.com>
2022-04-07 15:25:53 +0200

    Put escape char func in build

--- cli/build.v
+++ cli/build.v
@@ -1,11 +1,29 @@
 module cli
 
 import os
-import yaml
 import open_api
+import regex
+import yaml
+
+fn escape_escaped_char(str string) ?string {
+	mut tmp := str.clone()
+	mut checked := []string{}
+
+	mut reg := regex.regex_opt(r'\\[a-zA-Z]') ?
+	for char in reg.find_all_str(str) {
+		if char in checked {
+			continue
+		}
+		tmp = tmp.replace(char, '\\$char')
+		checked << char
+	}
+
+	return tmp
+}
 
 pub fn build(path string, debug bool) ?string {
-	content := os.read_file(path) ?
+	mut content := os.read_file(path) ?
+	content = escape_escaped_char(content) ?
 	raw_json := yaml.yaml_to_json(content, replace_tags: true, debug: int(debug)) ?
 	json_data := open_api.decode<open_api.OpenApi>(raw_json) ?
 	return json_data.str()
