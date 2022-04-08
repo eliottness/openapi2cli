51b99ba2189e1483355de6ce2f0a985db8831748
Eliott Bouhana <eliott.bouhana@epita.fr>
2022-04-08 16:06:30 +0200

    --wip-- [skip ci]

--- cli/build.v
+++ cli/build.v
@@ -7,8 +7,9 @@ import yaml
 fn render(operation_list []open_api.Operation, info_title string, info_description string) string {
 	operations := operation_list.clone()
 	title := info_title
+	name := info_title
 	description := info_description
-	return $tmpl('./templates/cli.tmpl')
+	return $tmpl('./templates/test_renderer.tmpl')
 }
 
 pub fn build(path string, debug bool) ?string {
@@ -16,9 +17,9 @@ pub fn build(path string, debug bool) ?string {
 	content = escape_escaped_char(content) ?
 	raw_json := yaml.yaml_to_json(content, replace_tags: true, debug: int(debug)) ?
 	open_api := open_api.decode<open_api.OpenApi>(raw_json) ?
-	program := render([open_api.paths['/batch'].get], open_api.info.title, open.info.description)
+	program := render([open_api.paths['/batch'].get], open_api.info.title, open_api.info.description)
 
-	os.write_file('./templated.v', program)
+	os.write_file('./templated.v', program) ?
 
-	return json_data.str()
+	return './templated.v'
 }
