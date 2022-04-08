efbebff890c93c537bbb8d4b618f92485a3d252d
alexandre.rulleau <rulleaualexandre@gmail.com>
2022-04-08 15:21:01 +0200

    modified template, we are not using generated annex functions anymore

--- cli/tests/renderer_test.v
+++ cli/tests/renderer_test.v
@@ -1,10 +1,36 @@
 import cli
+import open_api
+import yaml
+import os
+import regex
 
-fn render(data string) string {
-	operations := ['frere', 'soeur']
+fn escape_escaped_char(str string) ?string {
+	mut tmp := str.clone()
+	mut checked := []string{}
+
+	mut reg := regex.regex_opt(r'\\[a-zA-Z]') ?
+	for char in reg.find_all_str(str) {
+		if char in checked {
+			continue
+		}
+		tmp = tmp.replace(char, '\\$char')
+		checked << char
+	}
+
+	return tmp
+}
+
+fn render(operation_list []open_api.Operation) string {
+	operations := operation_list.clone()
+	title := 'title'
+	description := 'description'
 	return $tmpl('../templates/cli.tmpl')
 }
 
-fn test_render_hello_world() {
-	assert render('World') == 'Hello World\n'
+fn test_render_hello_world() ? {
+	mut content := os.read_file(@VMODROOT + '/open_api/testdata/open_api_complex.yaml') ?
+	content = escape_escaped_char(content) ?
+	raw_json := yaml.yaml_to_json(content, replace_tags: true) ?
+	open_api := open_api.decode<open_api.OpenApi>(raw_json) ?
+	assert render([open_api.paths['/batch'].get]) == 'Hello World\n'
 }
