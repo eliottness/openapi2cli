3979939f1aded6f8a33f87b711d13e777ba38263
Leiyks <rulleaualexandre@gmail.com>
2022-04-12 00:06:49 +0200

    Refacto(template) Finish merging body and body_stdin flags

--- cli_builder/utils.v
+++ cli_builder/utils.v
@@ -31,36 +31,29 @@ fn get_fetch_config(method string, url string, data string) http.FetchConfig {
 	}
 }
 
+fn get_stdin_input() ?string {
+	mut input := ''
+	for {
+		str := os.input_opt('') or { break }
+		input += str
+	}
+	return input
+}
+
 pub fn execute_command(method string, path string, content_types []string, cmd cli.Command) ? {
-	flags := cmd.flags.get_all_found()
-	flags_name := flags.map(fn (elt cli.Flag) string {
-		return elt.name
-	})
-
-	if 'body' in flags_name && 'body_stdin' in flags_name {
-		println("Error: You cant use 'body' and 'body_stdin' simultaneously")
-		return
-	}
-
-	if method in ['PUT', 'POST', 'PATCH'] && 'body' !in flags_name && 'body_stdin' !in flags_name {
-		println('Error: Specifying a body is mandatory for this operation.')
-		return
-	}
-
 	mut data := ''
 	mut url := path
-	for flag in flags {
-		if flag.flag == cli.FlagType.string {
-			if flag.name != 'body' {
-				url = url.replace('{' + flag.name + '}', flag.get_string() ?)
-			} else {
-				data = flag.get_string() ?
-			}
+
+	for flag in cmd.flags.get_all_found() {
+		if flag.name != 'body' {
+			url = url.replace('{' + flag.name + '}', flag.get_string() ?)
+		} else {
+			data = flag.get_string() ?
 		}
 	}
 
-	if 'body_stdin' in flags_name {
-		data = os.input('Enter request body:\n')
+	if data == '-' {
+		data = get_stdin_input() ?
 	} else if os.is_file(data) {
 		data = os.read_file(data) ?
 	}
