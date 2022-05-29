6a827b1e549987927329edcd8e8db0cd23c3209c
Eliott Bouhana <eliott.bouhana@epita.fr>
2022-05-29 17:13:27 +0200

    fix v fmt
    Signed-off-by: Eliott Bouhana <eliott.bouhana@epita.fr>

--- cli_builder/validate.v
+++ cli_builder/validate.v
@@ -4,19 +4,19 @@ import openapi
 
 pub fn validate(mut open_api openapi.OpenApi, server string) ? {
 	if server.len == 0 && open_api.servers.len == 0 {
-		eprintln("No servers specified at global level")
-		eprintln("Please note this tool does not support multiple servers")
-		return error("No servers defined")
+		eprintln('No servers specified at global level')
+		eprintln('Please note this tool does not support multiple servers')
+		return error('No servers defined')
 	}
 
 	mut operation_ids := []string{}
 	for path, path_item in open_api.paths {
 		for method, operation in path_item.operations {
-			if operation.operation_id == "" {
-				return error("OperationId is required for path: " + path + " method: " + method)
+			if operation.operation_id == '' {
+				return error('OperationId is required for path: ' + path + ' method: ' + method)
 			}
 			if operation_ids.contains(operation.operation_id) {
-				return error("Duplicate OperationId: " + operation.operation_id)
+				return error('Duplicate OperationId: ' + operation.operation_id)
 			}
 			operation_ids << operation.operation_id
 		}
@@ -24,7 +24,7 @@ pub fn validate(mut open_api openapi.OpenApi, server string) ? {
 
 	if server.len > 0 {
 		open_api.servers << openapi.Server{
-				url: server,
+			url: server
 		}
 	}
 }
